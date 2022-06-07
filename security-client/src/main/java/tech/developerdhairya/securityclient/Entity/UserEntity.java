87472f9855eaba6ab6837f510c0e22f2963f6281
developerdhairya <dhairya0192.be20@chitkara.edu.in>
2022-06-08 01:26:33 +0530

    Add UserEntity

--- security-client/src/main/java/tech/developerdhairya/securityclient/Entity/UserEntity.java
+++ security-client/src/main/java/tech/developerdhairya/securityclient/Entity/UserEntity.java
@@ -1,12 +1,19 @@
 package tech.developerdhairya.securityclient.Entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstname;
-    private
+    private String lastName;
+    private String email;
+    @Column(length = 60)
+    private String password;
+    private String role;
+    private boolean enabled;
 
 }
